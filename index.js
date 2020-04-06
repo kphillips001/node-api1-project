280ee6daae6e7a831af1965fe6aa6dfb223869cd
Kevin <knphillips01@gmail.com>
2020-04-06 16:54:20 -0500

    added middleware

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const server = express();
 
+// middleware
+server.use(express.json()); 
+
 const port = 6000;
 server.listen(port, () => console.log(`api listening on port ${port}`))
