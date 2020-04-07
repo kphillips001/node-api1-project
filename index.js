cf07e2d19ac90eaabf10830841c08e7eb29978d1
Kevin <knphillips01@gmail.com>
2020-04-06 20:08:31 -0500

    mvp

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
+// middleware
+server.use(express.json());
+
 let users = [
   {
-  id: "a_unique_id", // hint: use the shortid npm package to generate it
-  name: "Jane Doe", // String, required
-  bio: "Not Tarzan's Wife, another Jane",  // String, required
-  }
+    name: "test",
+    bio: "test bio",
+    id: 0,
+  },
 ];
 
-// middleware
-server.use(express.json()); 
-
-//endpoints
-server.post('/api/users', (req, res) => {
+// endpoints
+server.post("/api/users", (req, res) => {
   const user = req.body;
-  if(user.name === '' || user.bio === '') {
-    res.status(400).json({ errorMessage: "The users information could not be retrieved." })
+  if (user.name === "" || user.bio === "") {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a name and bio for the user" });
   } else {
     users.push(user);
-    res.status(201).json(user); 
+    res.status(201).json(user);
   }
   if (!user) {
-    res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database ",
+    });
   }
 });
 
-server.get('api/users', (req, res) => {
-  users 
-  ? res.status(200).json(users)
-  : res.status(500).json({ errorMessage: "The users information could not be retrieved." })
-})
+server.get("/api/users", (req, res) => {
+  users
+    ? res.status(200).json(users)
+    : res.status(500).json({
+        errorMessage: "The users insformation could not be retrieved",
+      });
+});
 
-server.get('api/users/:id', (req, res) => {
+server.get("/api/users/:id", (req, res) => {
   const id = req.params.id;
 
   const user = users.find((e) => e.id == id);
 
   user
-  ? res.json(200).json(user)
-  : res.json(404).json({ message: "The user with the specified ID does not exist." })
+    ? res.json(user)
+    : res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
 });
 
 server.delete("/api/users/:id", (req, res) => {
-  const id = req.params.id;
+  const id = Number(req.params.id);
 
   const user = users.find((e) => e.id == id);
   users = users.filter((e) => Number(e.id) !== id);
@@ -56,6 +64,7 @@ server.delete("/api/users/:id", (req, res) => {
         .json({ message: "The user with the specified ID does not exist." });
 });
 
-
-const port = 6000;
-server.listen(port, () => console.log(`api listening on port ${port}`))
+const port = 5001;
+server.listen(port, () =>
+  console.log(`\n\n*** api running on port ${port} *** \n\n`)
+);
